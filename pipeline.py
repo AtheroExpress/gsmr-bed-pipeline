cdbf98bc35bd055cbd7bc93ebdbd8748f86385fb
Lennart Landsmeer <lennart@landsmeer.email>
2020-03-19 17:55:44 +0100

    Add SLURM header for pipeline

--- pipeline.py
+++ pipeline.py
@@ -3,7 +3,12 @@ import config
 
 region_jobs = job_definitions.jobs_for_region(config.qtl_region)
 
-print('set -ex')
+print(r'''#!/usr/bin/env bash
+#SBATCH --time 15:00:00
+#SBATCH --mem 40G
+
+set -ex
+''')
 
 for job in region_jobs:
     print(job)
